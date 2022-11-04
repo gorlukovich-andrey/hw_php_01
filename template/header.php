ba68bff3f1e9bee1d92c84b5c62c9e2f71e4a10e
Gorlukovich Andrey <114007457+gorlukovich-andrey@users.noreply.github.com>
2022-11-04 16:12:49 +0300

    Add files via upload

---
+++ template/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+<title><? if($TITLE) echo "$TITLE"; ?></title>
+<meta charset="utf-8">
+<meta name="description" content="<? if($DESCRIPTION) echo "$DESCRIPTION"; ?>">
+<meta name="keywords" content="<? if($KEYWORDS) echo "$KEYWORDS"; ?>">
+<link rel="icon" type="image/x-icon" href="/img/favicon.ico">
+<link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+<div class="site"><main class="main">
+<div class="container">
+<div class="title">
+<h1><? if($H1) echo "$H1"; ?></h1>
+</div>
+<hr/>
